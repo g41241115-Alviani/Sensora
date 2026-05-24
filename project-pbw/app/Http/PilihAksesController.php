0fceddf6c3de5e2308febf20a2c57e4b200b4bc2
g41241115-Alviani <g41241115@student.polije.ac.id>
2026-05-24 09:53:16 +0700

    update data kedua

---
+++ project-pbw/app/Http/PilihAksesController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class PilihAksesController extends Controller
+{
+    public function index()
+    {
+        return view('pilihakses'); // resources/views/pilihakses.blade.php
+    }
+}
